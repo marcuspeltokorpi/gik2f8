4a12bc07d38dddb1194171807002e83073846405
Marcus Peltokorpi <marcus.peltokorpi@gmail.com>
2022-11-20 22:17:07 +0100

    genererar html-lista

--- script.js
+++ script.js
@@ -13,8 +13,7 @@ const bookList = [
     }
 ];
 /* const searchInput = document.children[0].children[1].children[1].children[1];*/
-const searchField = document.getElementById('searchField');
-console.log(searchField);
+//const searchField = document.getElementById('searchField');
  
 searchField.addEventListener('keyup', handleKeyPress);
 
@@ -23,6 +22,7 @@ function handleKeyPress(e) {
     Skicka värdet till searchBooks 
     searchBooks retunerar en filtrerad lista
     Filtrerade listan skickas till renderBookList*/
+
     searchBooks(e.target.value);
 }
 
@@ -40,12 +40,31 @@ function searchBooks(searchTerm) {
         }
     }
     renderBookList(filteredList);
-    /*Element i HTML-listan visas/döljs beroende på listans innehåll */
+
 
 }
 
 
-function renderBookList(list) {
-    console.log(list)
+function renderBookList(bookList) {
+    /*Element i HTML-listan visas/döljs beroende på listans innehåll */
+    let html = `<ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">`;
+        for(let i = 0; i < bookList.length; i++) {
+            html += `<li
+                        class="book-listitem mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b
+                        border-indigo-700 cursor-pointer">
+                    ${bookList[i].author} - ${bookList[i].title}
+                    </li>`;
+        
+
+    }
+    html += `</ul>`;
+    const existingElement = document.querySelector(".book-list");
+    console.log(existingElement);
+    const root  = document.getElementById('root');
+    if(existingElement){
+        root.removeChild(existingElement);
+    }
+    
+    root.insertAdjacentHTML('beforeend', html);
 }
 
