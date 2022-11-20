77b6ffcdf935f0d57e718f8d87c40a3e694f7fbc
Marcus Peltokorpi <marcus.peltokorpi@gmail.com>
2022-11-20 22:35:25 +0100

    Komponenter

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const bookList = [
     }
 ];
 /* const searchInput = document.children[0].children[1].children[1].children[1];*/
-//const searchField = document.getElementById('searchField');
+
  
 searchField.addEventListener('keyup', handleKeyPress);
 
@@ -47,24 +47,19 @@ function searchBooks(searchTerm) {
 
 function renderBookList(bookList) {
     /*Element i HTML-listan visas/döljs beroende på listans innehåll */
-    let html = `<ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">`;
-        for(let i = 0; i < bookList.length; i++) {
-            html += `<li
-                        class="book-listitem mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b
-                        border-indigo-700 cursor-pointer">
-                    ${bookList[i].author} - ${bookList[i].title}
-                    </li>`;
-        
+    BookList(bookList);
+   
 
-    }
-    html += `</ul>`;
     const existingElement = document.querySelector(".book-list");
     console.log(existingElement);
+
     const root  = document.getElementById('root');
     if(existingElement){
         root.removeChild(existingElement);
     }
+    if (bookList.length > 0) {
+        root.insertAdjacentHTML('beforeend', BookList(bookList));
+    }
     
-    root.insertAdjacentHTML('beforeend', html);
 }
 
